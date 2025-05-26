843acc07cb1b7eafc98d5618bec994b4e8e5f616
Akash Singh <133749800+akashsingh000@users.noreply.github.com>
2025-05-26 17:22:26 +0530

    appname change

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     apps: [
         {
-            name: 'blogs-crud',
+            name: 'users-auth',
             script: 'dist/app.js',
             watch: false,
             env: {
